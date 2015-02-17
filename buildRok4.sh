5b010fbe1523c66efc96f4d8a3f39c41f97ebe79
Thibbo <thibault.coupin@gmail.com>
2015-02-17 21:44:44 +0100

    Up rok4 to 0.19.0, use rok4 git

--- buildRok4.sh
+++ buildRok4.sh
@@ -1,14 +1,17 @@
 #!/bin/bash
 
+VERSION="0.19.0"
+
 if [[ ! -d /rok4 ]]
 then
 	echo "Please mount a volume at /rok4"
-	echo "ex : docker run -v ./rok4:/rok4 rok4-build"
+	echo "ex : docker run -v $(pwd)/rok4:/rok4 rok4-build"
 	exit 0
 fi
-wget http://www.rok4.org/data/src/rok4-src.zip
-unzip rok4-src.zip
-cd rok*
+wget https://github.com/rok4/rok4/archive/${VERSION}.zip
+unzip rok4-${VERSION}.zip
+chmod -R 777 rok4-${VERSION}
+cd rok4-${VERSION}
 mkdir build
 cd build
 cmake .. -DBUILD_BE4=FALSE -DBUILD_DOC=FALSE -DCMAKE_INSTALL_PREFIX=/rok4
